5cdf93a0c0941cd7097234ede9d20c051a278772
Aamer Shahzad <2241691+talentedaamer@users.noreply.github.com>
2019-01-05 12:57:43 +0500

    add application images

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,11 @@
 // firebase configurations
 var config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+  apiKey: "AIzaSyDvlL1FrVyZyhTxdpMYcp9coFWFuQkY_l0",
+  authDomain: "firereact-crud.firebaseapp.com",
+  databaseURL: "https://firereact-crud.firebaseio.com",
+  projectId: "firereact-crud",
+  storageBucket: "firereact-crud.appspot.com",
+  messagingSenderId: "291871336458"
 };
 
 export default config;
